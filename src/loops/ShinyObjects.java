152394f0e08a971bfaa68b9de3fcb7cb5ba49475
DavidAndBlake <League@Administrators-iMac.local>
2017-12-08 18:24:27 -0800

    Yearalive,shinyobjects
    .

--- src/loops/ShinyObjects.java
+++ src/loops/ShinyObjects.java
@@ -5,30 +5,42 @@ package loops;
 
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-
+import javax.swing.JOptionPane;
 
 public class ShinyObjects {
 	public static void main(String[] args) {
 
 		// 2. Ask the user how many shiny objects they want
-
+		String x = JOptionPane.showInputDialog("How many shiny objects do you want?");
 		// 3. Play the sound that many times
+		int y = Integer.parseInt(x);
+		for (int i = 0; i < y; i++) {
+			playMisterZee();
+		}
+	
+	// TODO Auto-generated method stub
 
-		// 1. Call the method below
+	// 1. Call the method below
 
 	}
+	// TODO Auto-generated method stub
+
+	// TODO Auto-generated method stub
+
+	// TODO Auto-generated method stub
+
+	String shinyobjects = "shiny-objects.wav";
 
 	public static void playMisterZee() {
-    	try {
- 
-   		 Clip clip = AudioSystem.getClip();
-   		 clip.open(AudioSystem.getAudioInputStream(ShinyObjects.class.getResource("shiny-objects.wav")));
-   		 clip.start();
-   		 Thread.sleep(3600);
-    	} catch (Exception ex) {
-        	ex.printStackTrace();
-    	}
-	}
+		try {
 
+			Clip clip = AudioSystem.getClip();
+			clip.open(AudioSystem.getAudioInputStream(ShinyObjects.class.getResource("shiny-objects.wav")));
+			clip.start();
+			Thread.sleep(3600);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
 
 }
--- src/loops/YearsAlive.java
+++ src/loops/YearsAlive.java
@@ -2,6 +2,8 @@ package loops;
 
 public class YearsAlive {
 public static void main(String[] args) {
-	int x = 0;
+	for(int i=2001;i<=2017;i++) {
+		System.out.println(i);
+	}
 }
 }
